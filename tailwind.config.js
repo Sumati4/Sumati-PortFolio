c6e60260d3fd10e0e86280c7c8fc80074cfae6a5
sumati4 <sumati3976@gmail.com>
2024-07-23 01:46:58 +0530

    updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,18 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: ["./src/**/*.{html,js}"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./index.html",          
+    "./src/**/*.{js,jsx,ts,tsx}", 
+  ],
   theme: {
     extend: {},
   },
